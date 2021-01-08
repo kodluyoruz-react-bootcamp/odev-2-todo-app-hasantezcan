ccbd0d28ce5001333996b5b8fbe5d161a5de8ab4
hasantezcan <hasantezcan77@gmail.com>
2021-01-08 22:40:56 +0300

    Add newTodo feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
+
+import NewTodo from "./components/NewTodo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [todos, setTodos] = useState([
+		{ content: "abbas" },
+		{ content: "abbas2" },
+	]);
+
+	return (
+		<div className="todoapp">
+			<header className="header">
+				<h1>TODOS</h1>
+				<NewTodo todos={todos} setTodos={setTodos}></NewTodo>
+			</header>
+
+			{todos.map((todo, i) => (
+				<li key={i}>{todo.content}</li>
+			))}
+
+			<footer class="info">
+				<p>Click to edit a todo</p>
+				<p>
+					Created by <a href="https://d12n.me/">Dmitry Sharabin</a>
+				</p>
+				<p>
+					Part of <a href="http://todomvc.com">TodoMVC</a>
+				</p>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
